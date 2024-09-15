24bd0416bdf7b672ee2f12788b9f69b13bd6f6ea
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-09-15 21:35:20 +0000

    p104

---
+++ src/TOOLS/CURSES/WERASE.C
@@ -0,0 +1,13 @@
+#include "cur.h"
+
+void werase( win )
+WINDOW	*win;
+{
+    clr_region( win->x_org, win->x_org + (win->x_size - 1),
+	        win->y_org, win->y_org + (win->y_size - 1),
+		win->attrib				 );
+
+    cmove( win->y_org, win->x_org );
+    win->row = 0;
+    win->col = 0;
+}
---
+++ src/TOOLS/CURSES/WINCH.C
@@ -0,0 +1,13 @@
+#include "cur.h"
+
+int	winch( win )
+WINDOW	*win;
+{
+    int	y, x, c;
+
+    curpos( &y, &x );
+    cmove( win->y_org + win->row, win->x_org + win->col );
+    c = inchar();
+    cmove( y, x );
+    return c;
+}
